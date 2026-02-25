209768c140f970420ea0aaad02142350e8100593
rajabmirzayev <rajabmirzayevv@gmail.com>
2026-02-25 23:17:15 +0400

    chore: initialize code structure

---
+++ src/main/java/com/rgb/foxwear/FoxWearApplication.java
@@ -0,0 +1,13 @@
+package com.rgb.foxwear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxWearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoxWearApplication.class, args);
+    }
+
+}
