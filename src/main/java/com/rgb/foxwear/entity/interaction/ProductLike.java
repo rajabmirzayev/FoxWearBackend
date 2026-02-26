db4ec363bdf4d1fe21a6ecfe80b697739e8bf4dd
rajabmirzayev <rajabmirzayevv@gmail.com>
2026-02-26 04:24:14 +0400

    fear: add Review models

--- src/main/java/com/rgb/foxwear/entity/interaction/ProductLike.java
+++ src/main/java/com/rgb/foxwear/entity/interaction/ProductLike.java
@@ -25,6 +25,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ProductLike {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
@@ -44,4 +45,5 @@ public class ProductLike {
     @Column(name = "liked_at", nullable = false, updatable = false)
     @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
     LocalDateTime likedAt;
+
 }
