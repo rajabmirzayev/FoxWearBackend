9774c4c04b991ca51a9e64fac9feb472517a476b
rajabmirzayev <rajabmirzayevv@gmail.com>
2026-02-27 08:55:02 +0400

    feat: add StringHelper utils

---
+++ src/main/java/com/rgb/foxwear/util/StringHelper.java
@@ -0,0 +1,17 @@
+package com.rgb.foxwear.util;
+
+public class StringHelper {
+
+    public static String capitalize(String text) {
+        text = text != null ? text.trim() : "";
+
+        return text.substring(0, 1).toUpperCase() +
+                text.substring(1).toLowerCase();
+    }
+
+    public static String tiUpper(String text) {
+        text = text != null ? text.trim() : "";
+
+        return text.toUpperCase();
+    }
+}
