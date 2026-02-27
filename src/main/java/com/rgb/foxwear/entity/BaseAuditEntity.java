bec41c0b72aa535d1e704f287c47d11ab992b440
rajabmirzayev <rajabmirzayevv@gmail.com>
2026-02-27 14:54:30 +0400

    chore: change timestamp in BaseAuditEntity

--- src/main/java/com/rgb/foxwear/entity/BaseAuditEntity.java
+++ src/main/java/com/rgb/foxwear/entity/BaseAuditEntity.java
@@ -2,31 +2,28 @@ package com.rgb.foxwear.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public abstract class BaseAuditEntity {
 
-    @CreatedDate
+    @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
     @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
     LocalDateTime createdAt;
 
-    @LastModifiedDate
+    @UpdateTimestamp
     @Column(name = "updated_at", nullable = false)
     @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
     LocalDateTime updatedAt;
