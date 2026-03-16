5f5a2fc808b9c7059802437e309e1c544647924c
rajabmirzayev <rajabmirzayevv@gmail.com>
2026-03-16 12:47:42 +0400

    feat: add swagger documentation to Dynamic controller

--- src/main/java/com/rgb/foxwear/controller/DynamicController.java
+++ src/main/java/com/rgb/foxwear/controller/DynamicController.java
@@ -3,6 +3,9 @@ package com.rgb.foxwear.controller;
 import com.rgb.foxwear.dto.ApiResponse;
 import com.rgb.foxwear.dto.response.dynamic.BannerResponse;
 import com.rgb.foxwear.service.DynamicService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/dynamic")
 @RequiredArgsConstructor
+@Tag(name = "Dynamic Management", description = "APIs for getting dynamic contents like banners")
 public class DynamicController {
     private final DynamicService dynamicService;
 
+    @Operation(summary = "Get banner by placement")
     @GetMapping("/banner")
     public ResponseEntity<@NonNull ApiResponse<BannerResponse>> getBanner(
-            @RequestParam String placement
+            @Parameter(description = "Placement of the banner") @RequestParam String placement
     ) {
         var response = dynamicService.getBanner(placement);
         return ResponseEntity.ok(ApiResponse.success(response));
